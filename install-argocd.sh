711c7312ee5650730c07f9962954a821f6311785
Karol Szpakowski <karol.szpakowski@gmail.com>
2021-04-24 14:03:22 +0200

    add app of apps bootstrap

--- install-argocd.sh
+++ install-argocd.sh
@@ -2,3 +2,4 @@
 
 kubectl create namespace argocd
 kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/v2.0.1/manifests/install.yaml
+kubectl apply -n argocd -f ./app-of-apps.yml
