67c3fce7c09a85357aef9d57d66302a57ee21c98
Karol Szpakowski <karol.szpakowski@nn.pl>
2021-06-27 14:49:40 +0200

    use app-of-apps repository version in install script

--- install-argocd.sh
+++ install-argocd.sh
@@ -2,4 +2,4 @@
 
 kubectl create namespace argocd
 kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/v2.0.1/manifests/install.yaml
-kubectl apply -n argocd -f ./app-of-apps.yml
+kubectl apply -n argocd -f https://raw.githubusercontent.com/kszpakowski/argo-apps/master/app-of-apps.yml
